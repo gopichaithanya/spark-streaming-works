302bca5686f465e162275c664c87185505dbb9c0
HadoopWorks <thirunagari.chaitu@gmail.com>
2020-01-31 16:54:24 +0530

    project-spark-practice examples

--- src/main/scala/spark/Spark02.scala
+++ src/main/scala/spark/Spark02.scala
@@ -1,5 +1,15 @@
 package spark
 
-object Spark02 {
+import org.apache.spark.sql.SparkSession
 
+object Spark02 {
+  def main(args: Array[String]): Unit = {
+    val spark=SparkSession.builder().appName("spark-app").master("local[*]").getOrCreate()
+    val data =  Array(12,13,56,45)
+    val r1= spark.sparkContext.parallelize(data,2)
+    val rtrans= r1.map(x=>x*10)
+    rtrans.collect()
+
+
+  }
 }
